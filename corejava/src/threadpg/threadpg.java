b58f308d67dba02799a08bb90afe72c5e503cfab
Akash Github <xyzx8920@gmail.com>
2022-05-22 21:45:03 +0530

    data members and member function

---
+++ corejava/src/threadpg/threadpg.java
@@ -0,0 +1,27 @@
+package threadpg;
+
+public class threadpg {
+	int year_of_pub;
+	String make;
+	int model;
+	double cost;
+
+	 public void disp1()
+	{
+	System.out.println("year of purchase"+2001);
+	System.out.println("make"+"suzuki");
+	}
+	 public void disp2()
+	 {
+		 System.out.println("model"+1999);
+		 System.out.println("cost"+25000.60);
+	 }
+	public static void main(String[] args) {
+ threadpg ob=new threadpg();
+ ob.disp1();
+ ob.disp2();
+}
+}
+	
+
+
