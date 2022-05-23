e7a6a70ea15e19927c2ceb12efd0632b3f12be1e
Akash Github <xyzx8920@gmail.com>
2022-05-23 15:17:07 +0530

    add

---
+++ corejava/src/abcpackage/Addition.java
@@ -0,0 +1,10 @@
+package abcpackage;
+
+public class Addition {
+
+	public int addTwoNumbers(int a,int b)
+	{
+	return a+b;
+	}
+
+}
---
+++ corejava/src/xyz/Test.java
@@ -0,0 +1,15 @@
+package xyz;
+
+import abcpackage.*;
+
+public class Test {
+	public static void main(String[]args)
+	{
+		Addition obj = new Addition();
+		
+		obj.addTwoNumbers(10,21);
+		System.out.println(obj.addTwoNumbers(10,21));
+		
+	}
+
+}
