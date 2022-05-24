a88ee483507cbd7f211052e8812971bc807776c4
Akash Github <xyzx8920@gmail.com>
2022-05-24 15:23:45 +0530

    token

---
+++ corejava/src/testt/Token.java
@@ -0,0 +1,15 @@
+package testt;
+
+import java.util.Scanner;
+
+public class Token {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner reader = new Scanner(System.in);
+System.out.println("enter a number");
+boolean b = reader.hasNextInt();
+System.out.println("You have entered:" + b);
+	}
+
+}
