1c0a5329bd88629beae71da456cba8c5b00b2b83
Akash Github <xyzx8920@gmail.com>
2022-05-25 21:35:13 +0530

    inherit

---
+++ corejava/src/Inheriting/Subclass.java
@@ -0,0 +1,22 @@
+package Inheriting;
+class Superclass
+{
+	void methodsuper()
+	{
+		System.out.println("I am super class");
+	}
+}
+class Subclass extends Superclass {
+	void methodSubclass()
+	{
+	System.out.println("I am sub clas method");
+	}
+}
+ class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Subclass a=new Subclass();
+a.methodsuper();
+a.methodSubclass();
+	}
+}
