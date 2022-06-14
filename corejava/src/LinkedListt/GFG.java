09204ff090c0b2a6b17de149220415dbf8b99643
Akash Github <xyzx8920@gmail.com>
2022-06-14 15:17:24 +0530

    Linked List

---
+++ corejava/src/LinkedListt/GFG.java
@@ -0,0 +1,17 @@
+package LinkedListt;
+import java.util.*;
+public class GFG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LinkedList<Integer> ll =new LinkedList<Integer>();
+for(int i=1;i<=5;i++)
+	ll.add(i);
+System.out.println(ll);
+ll.remove(3);
+System.out.println(ll);
+for(int i=0;i<ll.size();i++)
+	System.out.println(ll.get(i)+" ");
+	}
+
+}
