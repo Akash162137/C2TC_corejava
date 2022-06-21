baffb12a105e6232bf356af106a497eb0f0c0a3b
Akash Github <xyzx8920@gmail.com>
2022-06-21 19:25:29 +0530

    LINKED HASH SET

---
+++ corejava/src/linkedd/Linkedhashset.java
@@ -0,0 +1,18 @@
+package linkedd;
+import java.util.*;
+import java.util.HashSet;
+
+public class Linkedhashset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedHashSet<String >ob1 = new LinkedHashSet<String>();
+		ob1.add("Akash");
+		ob1.add("Rana");
+		ob1.add("Nithya");
+		ob1.add(null);
+		ob1.add(null);
+		System.out.println(ob1);
+	}
+
+}
