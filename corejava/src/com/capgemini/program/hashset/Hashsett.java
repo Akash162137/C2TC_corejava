5908c994bf4155dad9825598d4614a5eb535c0c6
Akash Github <xyzx8920@gmail.com>
2022-06-21 19:25:11 +0530

    HASH SET

---
+++ corejava/src/com/capgemini/program/hashset/Hashsett.java
@@ -0,0 +1,22 @@
+package com.capgemini.program.hashset;
+import java.util.*;
+
+public class Hashsett {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+HashSet<String >ob1 = new HashSet<String>();
+ob1.add("Akash");
+ob1.add("Rana");
+ob1.add("Nithya");
+ob1.add(null);
+ob1.add(null);
+//ob1.add("Akash");//
+/*Iterator<String>itr = ob1.iterator();
+//while(itr hasNext()) {
+	System.out.println(itr next());}*/
+
+System.out.println(ob1);
+	}
+
+}
