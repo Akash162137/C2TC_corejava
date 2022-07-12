1cbd336fa075508d64f8da6f1b284b9490e9b5dc
Akash Github <xyzx8920@gmail.com>
2022-07-12 12:16:47 +0530

    concatinate

---
+++ corejava/src/com/capgemini/program/threads/Akash.java
@@ -0,0 +1,12 @@
+package com.capgemini.program.threads;
+
+public class Akash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String str= "hello";
+str.concat("world");
+System.out.println(str);
+	}
+
+}
